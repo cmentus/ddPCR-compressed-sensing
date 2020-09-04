3de0ccd8f79c167aaa5db1fc5c23f5087863cd77
cmentus <cassidy.mentus@gmail.com>
2020-09-04 14:10:33 -0400

    Delete mutual_coherence.m

--- mutual_coherence.m
+++
@@ -1,7 +0,0 @@
-function mu = mutual_coherence(A)
-% First l2-normalize the columns of A. 
-% Compute the dot products using A*A (Gram matrix)
-A_S=bsxfun(@rdivide,A,sqrt(sum(A.^2,1)));
-mu=max(max(triu(transpose(A_S)*A_S,1)));
-end
-
